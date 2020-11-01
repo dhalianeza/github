702b3b7a2a065b2b66f73521e580ab13a49cbc25
dhalianeza <dhalianeza02@gmail.com>
2020-11-01 21:15:37 +0200

    add changes

--- Documents/Angular-app/Quotes/src/app/quote-detail/quote-detail.component.ts
+++ Documents/Angular-app/Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -17,6 +17,12 @@ export class QuoteDetailComponent implements OnInit {
   quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
+  upvote(){
+    this.quote.upvote+=1;
+  }
+  downvote(){
+    this.quote.downvote-=1;
+  }
   constructor() { }
 
   ngOnInit() {
